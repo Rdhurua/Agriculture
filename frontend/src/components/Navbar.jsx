9b83a146b1f6ad0322727aefee31633033234e9e
Rinku Dhurua <rinkudhurua258@gmail.com>
2025-07-30 22:19:37 +0530

    merging test

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
                     </button>
 
                     <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-dropdown">
-                        <ul className="flex flex-col items-center font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="flex flex-col items-center font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-200 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             {navItems.map(({ link, path }) => (
                                 <li key={path}>
                                     {location.pathname === '/' || location.pathname === '/home' ? (
